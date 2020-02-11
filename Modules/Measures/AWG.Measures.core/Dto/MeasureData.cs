5c0697c7143ec9dd1a612e6a2ec1da45568fdcd9
Daniele Belfiore <dev1@agricolus.com>
2020-02-11 09:40:04 +0100

    Added POST endpoint

---
+++ Modules/Measures/AWG.Measures.core/Dto/MeasureData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using AWG.Measures.Dto;
+
+namespace AWG.Measures.Core.Dto
+{
+  public class MeasureData
+  {
+    public string StationId { get; set; }
+    public DateTime Date { get; set; }
+    public SensorDataRaw[] Datas { get; set; }
+  }
+}
